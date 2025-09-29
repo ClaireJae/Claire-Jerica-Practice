58a7ad190f0fc9b10bae300b1d0c0295c5e3a087
Jerica <jericahanna04@gmail.com>
2025-09-28 20:47:17 -0500

    Adding the files to the repo help

---
+++ README.md
@@ -0,0 +1,24 @@
+# Claire-Jerica-Practice
+A creative website where we both experiment with different languages &amp; frameworks. 
+
+# What is the website going to be doing?
+- A combined but split website where we each can create our own pages regarding the same topics (Such as JS, C#, Python, etc.)
+- Will help us both grow as programmers and understand things better by working alongside a friend. 
+
+# CALENDAR FOR PLANNING - SUGGESTIONS ENCOURAGED
+- Week 1
+    - HTML/CSS basics
+- Week 2 
+    - HTML/CSS intermediate
+- Week 3 
+    - HTML/CSS slightly advanced
+- Week 4 
+    - Python basics
+- Week 5 
+    - JavaScript basics
+- Week 6 
+    - TypeScript basics
+- Week 7 
+    - Angular basics
+- Week 8 
+    - React basics
---
+++ js/script.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "https://static.vecteezy.com/system/resources/previews/021/352/964/original/trash-icon-recycle-and-trash-sign-symbol-icon-free-png.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
